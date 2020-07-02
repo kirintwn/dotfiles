1c0b75268581b980b789ed47e723d5a646295297
kirintwn <cy91244@gmail.com>
2020-07-03 01:34:37 +0800

    feat(zsh): add bat -p alias & tweaks printpath alias

--- ZSH_CONFIG/aliases.zsh
+++ ZSH_CONFIG/aliases.zsh
@@ -4,12 +4,13 @@ alias d="dotfiles"
 alias ls="exa --color=always --icons --group-directories-first"
 alias tree="exa --color=always --icons --group-directories-first -T --git-ignore"
 alias cat="bat"
+alias catp="bat -p"
 alias vi="nvim"
 alias vim="nvim"
 alias htop="glances"
 
 # print each PATH entry on a separate line
-alias printpath="echo -e ${PATH//:/$'\n'}"
+alias printpath='print -rl $path'
 
 # print my public ip
 alias publicip="dig +short myip.opendns.com @resolver1.opendns.com"
