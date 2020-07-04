239ebdfef5d6f2baf2a187d99f8ac053d6fe67b9
kirintwn <cy91244@gmail.com>
2020-07-04 18:19:00 +0800

    feat(asdf): add golang & rust plugins

--- scripts/install-packages/asdf.sh
+++ scripts/install-packages/asdf.sh
@@ -15,3 +15,9 @@ asdf plugin-add python
 
 # Add lua plugin
 asdf plugin-add lua https://github.com/Stratus3D/asdf-lua.git
+
+# Add golang plugin
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+
+# Add rust plugin
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
