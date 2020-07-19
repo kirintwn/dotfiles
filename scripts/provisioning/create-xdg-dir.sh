5ffd43a9a693fd519ae95ee0d888587183a36f46
kirintwn <cy91244@gmail.com>
2020-07-20 01:05:56 +0800

    feat(provisioning): add alacritty dir creation

--- scripts/provisioning/create-xdg-dir.sh
+++ scripts/provisioning/create-xdg-dir.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p "$XDG_CONFIG_HOME"/{asdf,aws,bat,docker,k9s,nvim,tealdeer,zsh}
+mkdir -p "$XDG_CONFIG_HOME"/{alacritty,asdf,aws,bat,docker,k9s,nvim,tealdeer,zsh}
 mkdir -p "$XDG_DATA_HOME"/{asdf,docker-machine,go,node,zinit,zlua,zsh}
 mkdir -p "$XDG_CACHE_HOME"/{tealdeer,zcompdump}
