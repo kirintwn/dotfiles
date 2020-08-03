58d089e5bd56d79f3bf3fc49bb442d05e6d1e0b6
kirintwn <cy91244@gmail.com>
2020-08-03 19:38:06 +0800

    fix(fzf): update fzf-tab command to fix trigger bug

--- .config/zsh/misc.zsh
+++ .config/zsh/misc.zsh
@@ -53,4 +53,16 @@ FZF_TAB_COMMAND=(
   '--query=$query'
   '--header-lines=$#headers'
 )
+FZF_TAB_COMMAND=(
+  fzf
+  --ansi   # Enable ANSI color support, necessary for showing groups
+  --expect='$continuous_trigger,$print_query' # For continuous completion and print query
+  '--color=hl:$(( $#headers == 0 ? 108 : 255 ))'
+  --nth=2,3 --delimiter='\x00'  # Don't search prefix
+  --layout=reverse --height='${FZF_TMUX_HEIGHT:=75%}'
+  --tiebreak=begin -m --bind=ctrl-space:toggle --cycle
+  '--query=$query'   # $query will be expanded to query string at runtime.
+  '--header-lines=$#headers' # $#headers will be expanded to lines of headers at runtime
+  --print-query
+)
 zstyle ":fzf-tab:*" command $FZF_TAB_COMMAND
