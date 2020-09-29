d7ac5faf9e06e1deed7c6e039980bfa980b01b80
kirintwn <cy91244@gmail.com>
2020-09-29 11:55:22 +0800

    fix(scripts): add missing provisioning files & dirs

--- scripts/provisioning/create-xdg-dir.sh
+++ scripts/provisioning/create-xdg-dir.sh
@@ -3,3 +3,6 @@
 mkdir -p "$XDG_CONFIG_HOME"/{alacritty,asdf,aws,bat,docker,k9s,nvim,tealdeer,zsh}
 mkdir -p "$XDG_DATA_HOME"/{asdf,docker-machine,go,node,nvim,zinit,zlua,zsh}
 mkdir -p "$XDG_CACHE_HOME"/{tealdeer,zcompdump}
+
+touch "$XDG_CONFIG_HOME"/aws/{config,credentials}
+mkdir -p "$HOME/.local/bin"
