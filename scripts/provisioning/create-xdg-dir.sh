024eb684f71efef8054ea84c9941eed6fcf0e6b4
kirintwn <cy91244@gmail.com>
2021-05-26 01:03:41 +0800

    fix(scripts): add default values for env

--- scripts/provisioning/create-xdg-dir.sh
+++ scripts/provisioning/create-xdg-dir.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+
 mkdir -p "$XDG_CONFIG_HOME"/{alacritty,asdf,aws,bat,docker,k9s,nvim,tealdeer,zsh}
 mkdir -p "$XDG_DATA_HOME"/{asdf,docker-machine,go,node,nvim,zinit,zlua,zsh}
 mkdir -p "$XDG_CACHE_HOME"/{tealdeer,zcompdump}
