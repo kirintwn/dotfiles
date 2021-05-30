9b587bc11f1797f4912fe8aa6033bcfca457dad2
kirintwn <cy91244@gmail.com>
2021-05-30 16:11:30 +0800

    feat(fnm): use fnm for nodejs management

--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -15,3 +15,7 @@ export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+
+if command -v fnm &> /dev/null; then
+  eval "$(fnm env)"
+fi
