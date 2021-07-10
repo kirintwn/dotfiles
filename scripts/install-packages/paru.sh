1a806919f753f6c49dc8d3bbf8e058d030697301
kirintwn <cy91244@gmail.com>
2021-07-11 01:56:41 +0800

    feat(scripts): update paru packages

--- scripts/install-packages/paru.sh
+++ scripts/install-packages/paru.sh
@@ -32,18 +32,28 @@ paru -S pavucontrol
 paru -S google-chrome
 paru -S alacritty
 paru -S visual-studio-code-bin
+# paru -S gnome-keyring
 paru -S telegram-desktop
+# curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
 paru -S spotify
 paru -S flameshot
 paru -S f5vpn
 
+paru -S subversion
+paru -S unzip
+paru -S zsh
+# chsh -s $(which zsh)
 paru -S bat
 paru -S exa
 paru -S fd
 paru -S ripgrep
 paru -S fzf
-paru -S git-delta-bin
+paru -S git-delta
 paru -S fnm-bin
+# fnm list-remote; fnm install vX.Y.Z; fnm default vX.Y.Z;
+paru -S python
+paru -S python-pip
+paru -S lua
 paru -S bind
 paru -S traceroute
 paru -S neofetch
