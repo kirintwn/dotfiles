940e9a3040fe677d095823256e0661ca751b36d0
kirintwn <cy91244@gmail.com>
2021-09-07 17:16:47 +0800

    fix(scripts): fix outdated brew.sh

--- scripts/install-packages/brew.sh
+++ scripts/install-packages/brew.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 
 # Prerequisites:
-# * Xcode CLT - xcode-select --install
 # * Homebrew - /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 brew update
 brew upgrade
-
-
-brew install zsh
-# To use zsh as default shell:
-# *	sudo sh -c "echo $(which zsh) >> /etc/shells" 
-# *	chsh -s $(which zsh)
 
 brew install git
 brew install subversion
 brew install less
 brew install gnupg
+brew install wget
 brew install neofetch
 brew install neovim
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install fnm
+brew install python
 brew install lua
 brew install bat
 brew install exa
@@ -28,6 +23,7 @@ brew install ripgrep
 brew install fzf
 brew install git-delta
 
+brew install terraform
 brew install awscli
 brew install kubernetes-cli
 brew install k9s
@@ -37,21 +33,17 @@ brew install croc
 
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code-nerd-font
-brew install --cask appcleaner
-brew install --cask yabai
-brew install --cask karabiner-elements
-brew install --cask bettertouchtool
-brew install --cask scroll-reverser
-brew install --cask balance-lock
 brew install --cask google-chrome
 brew install --cask visual-studio-code
-brew install --cask iterm2
-brew install --cask alacritty
 brew install --cask docker
-brew install --cask telegram
 brew install --cask spotify
+brew install --cask telegram
+brew install --cask alacritty
+brew install --cask balance-lock
+brew install --cask scroll-reverser
+brew install --cask karabiner-elements
+brew install --cask bettertouchtool
+brew install --cask yabai
 brew install --cask iina
 brew install --cask zoom
-brew install --cask microsoft-outlook
-
 brew cleanup
