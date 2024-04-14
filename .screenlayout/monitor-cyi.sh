889219e2052be54bdafcdb82a7c4c9a50d6414f9
kirintwn <me@kirintw.me>
2024-04-14 17:05:17 +0800

    chore: 2024.04 regular update

---
+++ .screenlayout/monitor-cyi.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output HDMI-0 --mode 2560x1440 --pos 2560x-280 --rotate right 
---
+++ .screenlayout/monitor-tpe.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2 --primary --mode 2560x1440 --rate 240 --pos 0x0 --rotate normal --output DP-0 --mode 2560x1440 --rate 144 --pos 2560x-500 --rotate right
