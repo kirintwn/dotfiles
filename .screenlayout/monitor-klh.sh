1cef064a767b6b9905f4e7e79a0cd72885bad828
kirintwn <me@kirintw.me>
2025-05-22 01:13:08 +0800

    chore: update monitor setup

--- .screenlayout/monitor-klh.sh
+++ .screenlayout/monitor-klh.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 2560x1440 --rate 240 --pos 0x0 --rotate normal --output DP-0 --mode 2560x1440 --rate 120 --pos 2560x-392 --rotate right
+xrandr --output DP-0 --primary --mode 2560x1440 --rate 360 --pos 0x0 --rotate normal --output DP-2 --mode 2560x1440 --rate 180 --pos 2560x-392 --rotate right
