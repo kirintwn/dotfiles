2476bc2b40cb3c90b96aea4e6adc263208453424
kirintwn <me@kirintw.me>
2025-08-01 00:51:08 +0800

    fix: fix macos ssh agent path in setup.zsh

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -76,7 +76,7 @@ rm -f "$HOME/.config/mimeapps.list"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
   mkdir -p "$HOME/.1password"
-  ln -s "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$HOME/.1password/agent.sock"
+  ln -s "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$HOME/.1password/agent.sock"
   sudo mkdir -p /opt/1password
   sudo ln -s /Applications/1Password.app/Contents/MacOS/op-ssh-sign /opt/1password/op-ssh-sign
   ln -s /usr/local/opt/openvpn/sbin/openvpn "$HOME/.local/bin/openvpn"
